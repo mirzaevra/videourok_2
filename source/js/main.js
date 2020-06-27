1743fbe37225013ef551a603586b8fbba9cc6e39
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-06-27 20:15:33 +0300

    reviews

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
 
-});
+})
 
 $(window).on('load', function () {
-
+    Common.reviewsSlider()
 })
 
 
 $(window).on('resize', function () {
 
 })
+
+Common = {
+    events: function () {
+
+    },
+    init: function () {
+    },
+    reviewsSlider: function () {
+        $('#reviews-slider .owl-carousel').owlCarousel({
+            items: 1,
+            slideBy: 1,
+            loop: true
+        })
+    }
+
+}
