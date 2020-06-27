4ae3f7481edff71e24043dc250befbd83ed282bb
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-06-27 22:49:45 +0300

    Лицензия

--- source/js/main.js
+++ source/js/main.js
@@ -3,8 +3,8 @@ $(window).on('load', function () {
         items: 1,
         slideBy: 1,
         loop: true,
-        autoplay: true,
-        autoplayTimeout: 10000
+        // autoplay: true,
+        // autoplayTimeout: 10000
     })
     coursSlider.init($('#reviews-slider'))
 })
