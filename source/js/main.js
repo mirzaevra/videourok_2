9e20c3cef95d93a186765f47262c5f6b44c67ee2
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-06-28 12:13:30 +0300

    parallax

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,23 @@
+$(document).ready(function () {
+
+    $(window).on('scroll', function () {
+        if ($('#possibility').height() + 100 > this.pageYOffset) {
+            var paralaxSpeed = .2
+            var offset = this.pageYOffset * paralaxSpeed
+            var paralaxElem = $('#possibility-img')
+            paralaxElem.css({
+                'transform': `translate3d(0,${offset}px,0)`
+            })
+        }
+    })
+
+})
+
 $(window).on('load', function () {
-    var coursSlider = new Slider({
+    const coursSlider = new Slider({
         items: 1,
         slideBy: 1,
-        loop: true,
+        loop: true
         // autoplay: true,
         // autoplayTimeout: 10000
     })
