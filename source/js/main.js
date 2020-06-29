1665667e8ac222c7f1de9fac1e02573df6a66f8e
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-06-29 18:53:41 +0300

    фкисы

--- source/js/main.js
+++ source/js/main.js
@@ -1,31 +1,55 @@
 $(document).ready(function () {
     $(window).on('scroll', function () {
-        if ($('#possibility').height() + 100 > this.pageYOffset) {
+        Common.parallaxEffect(this)
+        Common.floatTitle(this)
+    })
+})
+
+$(window).on('load', function () {
+    Common.reviewsSlider()
+})
+
+const Common = {
+    reviewsSlider() {
+        const reviewsSlider = new Slider({
+            items: 1,
+            slideBy: 1,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 10000,
+            responsive: {
+                0: {
+                    autoHeight: true
+                },
+                1024: {
+                    autoHeight: false
+                }
+            }
+        })
+        reviewsSlider.init($('#reviews-slider'))
+    },
+    parallaxEffect(self) {
+        if ($('#possibility').height() + 100 > self.pageYOffset) {
             var paralaxSpeed = .2
-            var offset = this.pageYOffset * paralaxSpeed
+            var offset = self.pageYOffset * paralaxSpeed
             var paralaxElem = $('#possibility-img')
             paralaxElem.css({
                 'transform': `translate3d(0,${offset}px,0)`
             })
         }
-    })
-})
+    },
+    floatTitle(self) {
+        if ($(window).width() <= 767) {
+            const parent = $('.tools')
+            const floatTtile = $('[data-float="title"]')
+            const parentTopPositon = parent.offset().top + floatTtile.height() * 1.3
+            const parentBottomPositon = parentTopPositon + parent.height() - floatTtile.height() * 3
 
-$(window).on('load', function () {
-    const coursSlider = new Slider({
-        items: 1,
-        slideBy: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 10000,
-        responsive: {
-            0: {
-                autoHeight: true
-            },
-            1024: {
-                autoHeight: false
+            if (self.pageYOffset >= parentTopPositon && self.pageYOffset <= parentBottomPositon) {
+                floatTtile.fadeIn()
+            } else {
+                floatTtile.fadeOut()
             }
         }
-    })
-    coursSlider.init($('#reviews-slider'))
-})
+    }
+}
